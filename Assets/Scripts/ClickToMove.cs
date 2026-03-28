9a1d8172b55e75fd292f4cebffd9edbc2c704c5e
Dhir Patel <dhirpatel768@gmail.com>
2026-03-27 22:55:18 -0600

    click to move + target locations

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI; // We need this to talk to the NavMesh!
 
 public class ClickToMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent agent;
+
     void Start()
     {
-        
+        // Grab the AI brain attached to our capsule
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 0 is the left mouse button
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            // Shoot an invisible laser from the camera through the mouse pointer
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the laser hits something solid (like our baked floor)...
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Tell the AI brain to drive to that exact spot!
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 }
